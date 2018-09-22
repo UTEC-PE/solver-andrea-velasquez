33a5ddfdc5d985788464ddb98592253471e4d636
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2018-09-22 03:49:50 -0500

    Add node.h with basic methods

---
+++ node.h
@@ -0,0 +1,38 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <cctype> //isdigit ()
+
+bool isOperator(char x);
+char sumOperator(char op1, char op2);
+
+struct Node {
+    char data;
+    Node* left;
+    Node* right;
+
+    Node(char data) : data(data) {
+        left = right = nullptr;
+    };
+
+    bool isOperator(){
+      return ::isOperator(this->data);
+    };
+    bool mergeOperator(char op){
+      if (!isOperator() ||  !(op=='+' || op=='-')) return false;
+      data = sumOperator(op, data);
+      return true;
+    }
+};
+
+bool isOperator(char x){
+  return x=='+' ||  x=='-' ||  x=='/' ||  x=='*' ||  x=='^';
+};
+
+char sumOperator(char op1, char op2){
+  //Different: +- or -+ = -
+  //Equal: ++ or -- = +
+  return op1==op2 ? '+' : '-';
+}
+
+#endif
