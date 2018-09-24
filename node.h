34a5add8fda4fcba5787affa3afeef4e0d82aae4
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2018-09-23 22:22:59 -0500

    Add evaluate() and clear()

--- node.h
+++ node.h
@@ -1,7 +1,7 @@
 #ifndef NODE_H
 #define NODE_H
 
-#include <cctype> //isdigit ()
+#include <cctype> //isdigit()
 #include <string>
 
 bool isOperator(char x);
@@ -14,6 +14,11 @@ struct Node {
 
     Node(std::string data) : data(data), left(nullptr), right(nullptr){};
 
+    void killSelf(){
+      if (left) left->killSelf();
+      if (right) right->killSelf();
+      delete this;
+    };
     bool isOperator(){
       return ::isOperator(this->data[0]);
     };
@@ -21,6 +26,12 @@ struct Node {
       if (!(data[0]=='+' || data[0]=='-')) return false; // data is * or / or ^
       data = sumOperator(op, data[0]);
       return true;
+    };
+    int hasLeafs(){ // 0: no leafs, 1: left leaf, 2: right leaf, 3: both leafs
+      if (isdigit(left->data[0]) && isdigit(right->data[0])) return 3;
+      if (isdigit(left->data[0])) return 1;
+      if (isdigit(right->data[0])) return 2;
+      return 0;
     }
 };
 
--- solver.h
+++ solver.h
@@ -10,9 +10,9 @@
 
 using namespace std;
 
-// Functions for node.h and parser
+//Some functions for node.h and parse.h
 bool isOperator(char x){
-  return x=='+' ||  x=='-' ||  x=='/' ||  x=='*' ||  x=='^';
+  return x=='+' ||  x=='-' ||  x=='/' ||  x=='*' ||  x=='^' || x=='(' || x==')';
 };
 
 class Solver {
@@ -26,6 +26,33 @@ class Solver {
         };
         Node* root;
         Node* current;
+
+        float operate(char op, float n1, float n2){
+          switch (op){
+            case '+': return n1+n2;
+            case '-': return n1-n2;
+            case '*': return n1*n2;
+            case '/': return n1/n2;
+            case '^': return pow(n1, n2);
+          }
+          throw "Can't operate";
+        }
+
+        float evaluate(Node* root){
+          if (isdigit(root->data[0])) return stof(root->data);
+          switch (root->hasLeafs()) {
+            //no numbers as children
+            case 0: return operate(root->data[0], evaluate(root->left), evaluate(root->right));
+            //left children is number
+            case 1: return operate(root->data[0], stof(root->left->data), evaluate(root->right));
+            //right children is number
+            case 2: return operate(root->data[0], evaluate(root->left), stof(root->right->data));
+            //both leafs
+            case 3: return operate(root->data[0], stof(root->left->data), stof(root->right->data));
+          }
+          throw "Error in evaluate method";
+        };
+
 
     public:
         Solver(): root(nullptr), current(nullptr){};
@@ -50,7 +77,7 @@ class Solver {
             // current node has an operator
           if (current->isOperator()){
             if (!(c[0]=='+' || c[0]=='-')) return false; //Can't merge this operators
-            if (!current->mergeOperator(c[0])){ // current has * or / or ^
+            if (!current->mergeOperator(c[0])){ // current has * or / or ^ (ex: 3*-5 or 3*+5)
               current->right = newnode;
               current = current->right; // descend to right
               current->left = new Node("0"); // set left node to 0 (case: 5*-3)
@@ -73,15 +100,22 @@ class Solver {
           return true;
         };
 
-        bool parse(char* expression){
+        float evaluate(){
+          float ans = evaluate(root);
+          clear();
+          return ans;
+        };
+
+        int parse(char* expression){
           map<char, float> var;
           string number = "";
+          int counter = 0;
           for (char* c=expression; c!= expression + strlen(expression); c++){
             // c is operand
             if (isOperator(*c)) this->insert(string(1, *c));
             // c is digit or point (part of a number)
             else if (isdigit(*c) || *c=='.') {
-              if (*c=='.' && !isdigit(*(c+1))) return false; //incomplete decimal
+              if (*c=='.' && !isdigit(*(c+1))) return counter; //incomplete decimal
               number+=*c;
 
               // if next character isn't part of a number, end number
@@ -92,7 +126,7 @@ class Solver {
             }
             else{
               // variable must be between operators
-              if ( !isOperator(*(c-1)) || !isOperator(*(c+1)) ) return false;
+              if ( !isOperator(*(c-1)) || (!isOperator(*(c+1)) && counter!=strlen(expression)-1) ) return counter;
               //known variable
               if (var.count(*c)) this->insert(to_string(var[*c]));
               else{
@@ -101,12 +135,26 @@ class Solver {
                 this->insert(to_string(var[*c]));
               }
             }
-        }
-        return true;
+            ++counter;
+          }
+        return -1;
       };
 
-        ~Solver();
-      double solve(char* expression);
+
+      float solve(char* expression){
+        int errorAt = parse(expression);
+        if (errorAt!=-1) throw "\nError in character '" + string(1,expression[errorAt]) + "' at index " + to_string(errorAt);
+        return evaluate(root);
+      };
+
+      void clear(){
+        if (root) root->killSelf();
+        root=current=nullptr;
+      };
+
+      ~Solver(){
+        clear();
+      };
 };
 
 #endif
