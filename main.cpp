34e47fecc9ca09cec88d77ac7a814b015786ce56
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2018-09-23 23:32:07 -0500

    Add solver and new test cases in main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "solver.h"
 
 using namespace std;
 
@@ -8,10 +9,17 @@ void menu() {
       cout<<"\t\tPractica Operaciones"<<endl;
       cout<<"==========================================================="<<endl;
 
+      //Note: Variables can be floats
+
       //char* str="7/4*((a+b)*a)+3";
       //char* str="7/4*((a+-+--b)*a)+3";
-      //char* str="2+(3)";
-      char* str="7/4*((a+b)^4*a)+3";
+      //char str[]="2+(3)";
+      char str[]="2.4+(3+z)";
+      //char* str="7/4*((a+b)^4*a)+3";
+
+      Solver calculator;
+      //char str[]= "(3+(4))*3";
+      cout << "Answer: " << calculator.solve(str);
 }
 
 int main(int argc, char const *argv[]) {
